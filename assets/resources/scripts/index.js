7425af226e402aad40c9a149d45c6d430445781d
Luan Gabriel Dierk <112535830+Luangabrield@users.noreply.github.com>
2022-09-15 02:36:39 -0300

    inicio do site

---
+++ assets/resources/scripts/index.js
@@ -0,0 +1,3 @@
+var apelido = prompt('digite o apelido que deseja ser tratado aqui: ')
+confirm(`vamos chamar você de ${apelido} :)`)
+alert(`prazer em te conhecer ${apelido}!! Fique a vontade para apreciar nosso menu`)
